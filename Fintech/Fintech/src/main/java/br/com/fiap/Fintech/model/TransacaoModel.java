99d4508321dd4f5a58ff4485524ce123e2101877
Miguelhenri09 <miguelhenrique444@gmail.com>
2025-10-25 15:21:19 -0300

    Declarando as variáveis e adicionado os get seters

--- Fintech/Fintech/src/main/java/br/com/fiap/Fintech/model/TransacaoModel.java
+++ Fintech/Fintech/src/main/java/br/com/fiap/Fintech/model/TransacaoModel.java
@@ -1,4 +1,43 @@
 package br.com.fiap.Fintech.model;
 
+import java.time.LocalDate;
+
 public class TransacaoModel {
+
+    private Long id;
+    private double valor;
+    private LocalDate dataTransacao;
+    private String tipoTransacao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getDataTransacao() {
+        return dataTransacao;
+    }
+
+    public void setDataTransacao(LocalDate dataTransacao) {
+        this.dataTransacao = dataTransacao;
+    }
+
+    public String getTipoTransacao() {
+        return tipoTransacao;
+    }
+
+    public void setTipoTransacao(String tipoTransacao) {
+        this.tipoTransacao = tipoTransacao;
+    }
 }
